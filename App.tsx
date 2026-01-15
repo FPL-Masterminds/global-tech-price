d9f5173d609f295540e6165805688c0decbc517f
FPL-Masterminds <fantasyleaguemasterminds@gmail.com>
2026-01-15 23:17:13 +0000

    Test video #5 in footer, video #6 in hero

--- App.tsx
+++ App.tsx
@@ -12,11 +12,11 @@ const App: React.FC = () => {
   // Testing videos systematically - no randomization
   const sectionVideos = useMemo(() => {
     return {
-      hero: VIDEO_POOL[4],  // Video #5 (testing)
+      hero: VIDEO_POOL[5],  // Video #6 (testing)
       grid: VIDEO_POOL[1],
       card1: VIDEO_POOL[2],
       card2: VIDEO_POOL[3],
-      footer: VIDEO_POOL[0]  // Video #1 (FINAL - locked)
+      footer: VIDEO_POOL[4]  // Video #5 (testing in footer)
     };
   }, []);
 
