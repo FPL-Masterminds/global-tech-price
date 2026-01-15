7ce82899b0d15f9d16b97f03fd5ababb62a0c582
FPL-Masterminds <fantasyleaguemasterminds@gmail.com>
2026-01-15 23:11:03 +0000

    Test video #4 in hero

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
   // Testing videos systematically - no randomization
   const sectionVideos = useMemo(() => {
     return {
-      hero: VIDEO_POOL[2],  // Video #3 (testing)
+      hero: VIDEO_POOL[3],  // Video #4 (testing)
       grid: VIDEO_POOL[1],
       card1: VIDEO_POOL[2],
       card2: VIDEO_POOL[3],
