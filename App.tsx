508be5c3cdab63e2c581592dcd0d49ef04db8a07
FPL-Masterminds <fantasyleaguemasterminds@gmail.com>
2026-01-16 20:50:09 +0000

    Add Link import to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect, useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import VideoBackground from './components/VideoBackground';
 import { VIDEO_POOL, PRODUCTS, MOCK_PRICES } from './constants';
 import { Product } from './types';
