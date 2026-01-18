9d117a23de1a458fba842b4ab4dd7cf6aadfd7ff
FPL-Masterminds <fantasyleaguemasterminds@gmail.com>
2026-01-18 14:08:31 +0000

    Fix font to Lucida Grande - iTunes 2005 style

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,13 +6,8 @@ export default function Document() {
       <Head>
         {/* HLS.js for video streaming */}
         <script src="https://cdn.jsdelivr.net/npm/hls.js@1"></script>
-        
-        {/* Google Fonts - Inter */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
       </Head>
-      <body className="bg-black text-white" style={{ fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif' }}>
+      <body className="bg-black text-white" style={{ fontFamily: '"Lucida Grande", "Lucida Sans Unicode", Geneva, Verdana, sans-serif' }}>
         <Main />
         <NextScript />
       </body>
